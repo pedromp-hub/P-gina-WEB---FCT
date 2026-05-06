a94532fe9582a18ebcbf78278362bc4aba47d040
pedromp-hub <pmonpel2304@g.educaand.es>
2026-05-06 12:22:35 +0200

    sobre mi casi terminado a falta de retoques

--- prueba1.js
+++ prueba1.js
@@ -1,14 +1,19 @@
-const text = "armonik";
-    const speed = 150; // Velocidad en milisegundos por letra
-    const element = document.getElementById("typewriter");
-    let i = 0;
+const text = "armonik ";
+const speed = 150;
+const element = document.getElementById("typewriter");
+let i = 0;
 
-    function typeWriter() {
-        if (i < text.length) {
+function typeWriter() {
+    if (i < text.length) {
+        if (i === text.length - 1) {
+            element.classList.add("show-dot");
+        } else {
             element.innerHTML += text.charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
         }
+        
+        i++;
+        setTimeout(typeWriter, speed);
     }
-    
-    window.onload = typeWriter;
+}
+
+window.onload = typeWriter;
